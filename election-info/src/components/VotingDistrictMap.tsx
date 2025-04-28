a52428b068af0ec212a5e9108c24f6f5f3169aca
johnilluminati <johnbilluminati@gmail.com>
2025-04-28 01:40:57 -0400

    Leaflet map now populates all GeoJSON data, but is very slow to load

--- election-info/src/components/VotingDistrictMap.tsx
+++ election-info/src/components/VotingDistrictMap.tsx
@@ -10,14 +10,86 @@ interface VotingDistrictProperties {
 }
 
 export default function VotingDistrictMap() {
-  const [districts, setDistricts] = useState<GeoJsonObject | null>(null);
+  const [geoJsonLayers, setGeoJsonLayers] = useState<GeoJsonObject[]>([]);
 
   useEffect(() => {
-    fetch('/data/voting_districts.geojson')
-      .then((res) => res.json())
-      .then((data: GeoJsonObject) => setDistricts(data))
-      .catch((error) => {
-        console.error('Error loading voting districts:', error);
+    // List of GeoJSON files to load
+    const geoJsonFiles = [
+      '/data/alabama_voting_districts.geojson',
+      '/data/alaska_voting_districts.geojson',
+      '/data/american_samoa_voting_districts.geojson',
+      '/data/arizona_voting_districts.geojson',
+      '/data/arkansas_voting_districts.geojson',
+      '/data/california_voting_districts.geojson',
+      '/data/colorado_voting_districts.geojson',
+      '/data/commonwealth_of_northern_mariana_islands_voting_districts.geojson',
+      '/data/connecticut_voting_districts.geojson',
+      '/data/delaware_voting_districts.geojson',
+      '/data/district_of_columbia_voting_districts.geojson',
+      '/data/florida_voting_districts.geojson',
+      '/data/georgia_voting_districts.geojson',
+      '/data/guam_voting_districts.geojson',
+      '/data/hawaii_voting_districts.geojson',
+      '/data/idaho_voting_districts.geojson',
+      '/data/illinois_voting_districts.geojson',
+      '/data/indiana_voting_districts.geojson',
+      '/data/iowa_voting_districts.geojson',
+      '/data/kansas_voting_districts.geojson',
+      '/data/kentucky_voting_districts.geojson',
+      '/data/louisiana_voting_districts.geojson',
+      '/data/maine_voting_districts.geojson',
+      '/data/maryland_voting_districts.geojson',
+      '/data/massachusetts_voting_districts.geojson',
+      '/data/michigan_voting_districts.geojson',
+      '/data/minnesota_voting_districts.geojson',
+      '/data/mississippi_voting_districts.geojson',
+      '/data/missouri_voting_districts.geojson',
+      '/data/montana_voting_districts.geojson',
+      '/data/nebraska_voting_districts.geojson',
+      '/data/nevada_voting_districts.geojson',
+      '/data/new_hampshire_voting_districts.geojson',
+      '/data/new_jersey_voting_districts.geojson',
+      '/data/new_mexico_voting_districts.geojson',
+      '/data/new_york_voting_districts.geojson',
+      '/data/north_carolina_voting_districts.geojson',
+      '/data/north_dakota_voting_districts.geojson',
+      '/data/ohio_voting_districts.geojson',
+      '/data/oklahoma_voting_districts.geojson',
+      '/data/oregon_voting_districts.geojson',
+      '/data/pennsylvania_voting_districts.geojson',
+      '/data/puerto_rico_voting_districts.geojson',
+      '/data/rhode_island_voting_districts.geojson',
+      '/data/south_carolina_voting_districts.geojson',
+      '/data/south_dakota_voting_districts.geojson',
+      '/data/tennessee_voting_districts.geojson',
+      '/data/texas_voting_districts.geojson',
+      '/data/utah_voting_districts.geojson',
+      '/data/vermont_voting_districts.geojson',
+      '/data/virgin_islands_voting_districts.geojson',
+      '/data/virginia_voting_districts.geojson',
+      '/data/washington_voting_districts.geojson',
+      '/data/west_virginia_voting_districts.geojson',
+      '/data/wisconsin_voting_districts.geojson',
+      '/data/wyoming_voting_districts.geojson'
+    ];
+
+    // Fetch all GeoJSON files
+    Promise.all(
+      geoJsonFiles.map(file =>
+        fetch(file)
+          .then(res => res.json())
+          .catch(error => {
+            console.error(`Error loading ${file}:`, error);
+            return null;
+          })
+      )
+    )
+      .then(results => {
+        // Filter out any failed loads and set the state
+        setGeoJsonLayers(results.filter((result): result is GeoJsonObject => result !== null));
+      })
+      .catch(error => {
+        console.error('Error loading GeoJSON files:', error);
       });
   }, []);
 
@@ -45,7 +117,9 @@ export default function VotingDistrictMap() {
         attribution="&copy; OpenStreetMap contributors"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {districts && <GeoJSON data={districts} onEachFeature={onEachDistrict} />}
+      {geoJsonLayers.map((layer, index) => (
+        <GeoJSON key={index} data={layer} onEachFeature={onEachDistrict} />
+      ))}
     </MapContainer>
   );
 }
